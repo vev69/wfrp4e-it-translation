be3a0ac6535282083a8c1132f929ec45eb4d8cba
Fabio Giannace <fabio.giannace@gmail.com>
2023-01-04 20:41:23 +0100

    fix babele js

--- babele-register.js
+++ babele-register.js
@@ -1,13 +1,9 @@
-
 Hooks.once('init', () => {
-
-	if(typeof Babele !== 'undefined') {
-		
+	if (typeof Babele !== 'undefined') {
 		Babele.get().register({
-			module: 'FoundryVTT-wfrp4e-it',
+			module: 'wfrp4e-it-translation',
 			lang: 'it',
-			dir: 'compendium'
+			dir: 'compendium',
 		});
-
 	}
 });
