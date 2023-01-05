3eae0b7c1265d3deda8d93ec88e82df5b99e4416
Fabio Giannace <fabio.giannace@gmail.com>
2023-01-05 16:33:07 +0100

    add custom converter for skills

--- babele-register.js
+++ babele-register.js
@@ -1,9 +1,213 @@
+var skills = {
+	'Animal Care': 'Allevare',
+	'Animal Training ( )': 'Addestrare ( )',
+	'Animal Training (Demigryph)': 'Addestrare (Semigrifone)',
+	'Animal Training (Dog)': 'Addestrare (Cane)',
+	'Animal Training (Horse)': 'Addestrare (Cavallo)',
+	'Animal Training (Pegasus)': 'Addestrare (Pegaso)',
+	'Animal Training (Pigeon)': 'Addestrare (Piccione)',
+	'Art ( )': 'Arte ( )',
+	'Art (Cartography)': 'Arte (Cartografia)',
+	'Art (Engraving)': 'Arte (Incisore)',
+	'Art (Mosaics)': 'Arte (Mosaico)',
+	'Art (Painting)': 'Arte (Pittura)',
+	'Art (Sculpture)': 'Arte (Scultura)',
+	'Art (Tattoo)': 'Arte (Tatuaggi)',
+	'Art (Weaving)': 'Arte (Tessitura)',
+	Athletics: 'Atletica',
+	Bribery: 'Corrompere',
+	Channelling: 'Incanalare',
+	'Channelling ( )': 'Incanalare ( )',
+	'Channelling (Aqshy)': 'Incanalare (Aqshy)',
+	'Channelling (Azyr)': 'Incanalare (Azyr)',
+	'Channelling (Chamon)': 'Incanalare (Chamon)',
+	'Channelling (Dhar)': 'Incanalare (Dhar)',
+	'Channelling (Ghur)': 'Incanalare (Ghur)',
+	'Channelling (Ghyran)': 'Incanalare (Ghyran)',
+	'Channelling (Hysh)': 'Incanalare (Hysh)',
+	'Channelling (Shyish)': 'Incanalare (Shyish)',
+	'Channelling (Ulgu)': 'Incanalare (Ulgu)',
+	Charm: 'Affascinare',
+	'Charm Animal': 'Domare Animali',
+	Climb: 'Arrampicarsi',
+	'Consume Alcohol': 'Reggere Alcolici',
+	Cool: 'Freddezza',
+	Dodge: 'Schivare',
+	Drive: 'Condurre Carri',
+	Endurance: 'Tempra',
+	'Entertain ()': 'Intrattenere ()',
+	'Entertain (Comedy)': 'Intrattenere (Commedia)',
+	'Entertain (Singing)': 'Intrattenere (Canto)',
+	'Entertain (Storytelling)': 'Intrattenere (Cantastorie)',
+	Evaluate: 'Valutare',
+	Gamble: "Giocare d'Azzardo",
+	Gossip: 'Pettegolezzi',
+	Haggle: 'Mercanteggiare',
+	Heal: 'Guarire',
+	Intimidate: 'Intimidire',
+	Intuition: 'Intuire',
+	'Language ()': 'Linguaggio ()',
+	'Language (Albion)': 'Linguaggio (Albionese)',
+	'Language (Battle Tongue)': 'Linguaggio (Militare)',
+	'Language (Bretonnian)': 'Linguaggio (Bretonniano)',
+	'Language (Classical)': 'Linguaggio (Classico)',
+	'Language (Elthárin)': 'Linguaggio (Elthárin)',
+	'Language (Estalian)': 'Linguaggio (Estaliano)',
+	'Language (Gospodarinyi)': 'Linguaggio (Gospodarinyi)',
+	'Language (Grumbarth)': 'Linguaggio (Grumbarth)',
+	'Language (Khazalid)': 'Linguaggio (Khazalid)',
+	'Language (Magick)': 'Linguaggio (Magico)',
+	'Language (Mootish)': 'Linguaggio (Contradano)',
+	'Language (Norse)': 'Linguaggio (Norscano)',
+	'Language (Queekish)': 'Linguaggio (Queekish)',
+	'Language (Reikspiel)': 'Linguaggio (Reikspiel)',
+	'Language (Thieves Tongue)': 'Linguaggio (Gergo dei Ladri)',
+	'Language (Tilean)': 'Linguaggio (Tileano)',
+	'Language (Wastelander)': 'Linguaggio (Marienburghese)',
+	Leadership: 'Comandare',
+	'Lore ()': 'Conoscenza ()',
+	'Lore (Engineering)': 'Conoscenza (Ingegneria)',
+	'Lore (Geology)': 'Conoscenza (Geologia)',
+	'Lore (Heraldry)': 'Conoscenza (Araldica)',
+	'Lore (History)': 'Conoscenza (Storia)',
+	'Lore (Law)': 'Conoscenza (Legge)',
+	'Lore (Magick)': 'Conoscenza (Magia)',
+	'Lore (Metallurgy)': 'Conoscenza (Metallurgia)',
+	'Lore (Science)': 'Conoscenza (Scienza)',
+	'Lore (Theology)': 'Conoscenza (Teologia)',
+	'Melee ()': 'Mischia ()',
+	'Melee (Basic)': 'Mischia (Armi Base)',
+	'Melee (Brawling)': 'Mischia (Rissa)',
+	'Melee (Cavalry)': 'Mischia (Armi da Cavalleria)',
+	'Melee (Fencing)': 'Mischia (Scherma)',
+	'Melee (Flail)': 'Mischia (Flagelli)',
+	'Melee (Parry)': 'Mischia (Armi Difensive)',
+	'Melee (Polearm)': 'Mischia (Armi in Asta)',
+	'Melee (Two-Handed)': 'Mischia (Armi a Due Mani)',
+	Navigation: 'Orientarsi',
+	'Outdoor Survival': 'Sopravvivere',
+	Perception: 'Percepire',
+	'Perform ()': 'Esibirsi ()',
+	'Perform (Acrobatics)': 'Esibirsi (Acrobazia)',
+	'Perform (Clowning)': 'Esibirsi (Clown)',
+	'Perform (Dancing)': 'Esibirsi (Danza)',
+	'Perform (Firebreathing)': 'Esibirsi (Mangiafuoco)',
+	'Perform (Juggling)': 'Esibirsi (Giocoliere)',
+	'Perform (Miming)': 'Esibirsi (Mimo)',
+	'Perform (Rope Walking)': 'Esibirsi (Funambolo)',
+	'Pick Lock': 'Scassinare',
+	'Play ()': 'Suonare ()',
+	'Play (Bagpipe)': 'Suonare (Cornamusa)',
+	'Play (Harpsichord)': 'Suonare (Arpicordo)',
+	'Play (Horn)': 'Suonare (Corno)',
+	'Play (Lute)': 'Suonare (Liuto)',
+	'Play (Violin)': 'Suonare (Violino)',
+	Pray: 'Pregare',
+	'Ranged ()': 'Mira ()',
+	'Ranged (Blackpowder)': 'Mira (Armi da Fuoco)',
+	'Ranged (Bow)': 'Mira (Archi)',
+	'Ranged (Crossbow)': 'Mira (Balestre)',
+	'Ranged (Engineering)': 'Mira (Armi Sperimentali)',
+	'Ranged (Entangling)': 'Mira (Armi Intralcianti)',
+	'Ranged (Explosives)': 'Mira (Armi Esplosive)',
+	'Ranged (Sling)': 'Mira (Frombole)',
+	'Ranged (Throwing)': 'Mira (Armi da Lancio)',
+	Research: 'Ricercare',
+	'Ride ()': 'Cavalcare ()',
+	'Ride (Demigryph)': 'Cavalcare (Semigrifone)',
+	'Ride (Great Wolf)': 'Cavalcare (Lupo Gigante)',
+	'Ride (Griffon)': 'Cavalcare (Grifone)',
+	'Ride (Horse)': 'Cavalcare (Cavallo)',
+	'Ride (Pegasus)': 'Cavalcare (Pegaso)',
+	Row: 'Remare',
+	'Sail ()': 'Navigare ()',
+	'Sail (Barge)': 'Navigare (Chiatta)',
+	'Sail (Caravel)': 'Navigare (Caravella)',
+	'Sail (Cog)': 'Navigare (Cocca)',
+	'Sail (Frigate)': 'Navigare (Fregata)',
+	'Sail (Wolfship)': 'Navigare (Navi Lupo)',
+	'Secret Signs ()': 'Segni Segreti ()',
+	'Secret Signs (Grey Order)': 'Segni Segreti (Ordine Grigio)',
+	'Secret Signs (Guilder)': 'Segni Segreti (Gilde)',
+	'Secret Signs (Ranger)': 'Segni Segreti (Viandanti)',
+	'Secret Signs (Scout)': 'Segni Segreti (Scout)',
+	'Secret Signs (Thief)': 'Segni Segreti (Ladri)',
+	'Secret Signs (Vagabond)': 'Segni Segreti (Vagabondi)',
+	'Set Trap': 'Piazzare Trappole',
+	'Sleight of Hand': 'Rapidità di Mano',
+	'Stealth ()': 'Nascondersi ()',
+	'Stealth (Rural)': 'Nascondersi (Rurale)',
+	'Stealth (Underground)': 'Nascondersi (Sotterraneo)',
+	'Stealth (Urban)': 'Nascondersi (Urbano)',
+	Swim: 'Nuotare',
+	Track: 'Seguire Tracce',
+	'Trade ()': 'Mestiere ()',
+	'Trade (Apothecary)': 'Mestiere (Apotecario)',
+	'Trade (Calligrapher)': 'Mestiere (Calligrafo)',
+	'Trade (Carpenter)': 'Mestiere (Carpentiere)',
+	'Trade (Chandler)': 'Mestiere (Candelaio)',
+	'Trade (Cook)': 'Mestiere (Cuoco)',
+	'Trade (Embalmer)': 'Mestiere (Imbalsamatore)',
+	'Trade (Smith)': 'Mestiere (Fabbro)',
+	'Trade (Tanner)': 'Mestiere (Conciatore)',
+};
+
 Hooks.once('init', () => {
 	if (typeof Babele !== 'undefined') {
 		Babele.get().register({
 			module: 'wfrp4e-it-translation',
 			lang: 'it',
 			dir: 'compendium',
+		});
+
+		Babele.get().registerConverters({
+			convertSkills: (values) => {
+				let data = [];
+				values.map((skill) => {
+					data.push(skills[skill] ? skills[skill] : skill);
+					// let skillMapping = new CompendiumMapping('skills', skill);
+					// return function (items, translations) {
+					// 	return items.map((data) => {
+					// 		if (translations) {
+					// 			let translation;
+					// 			if (Array.isArray(translations)) {
+					// 				translation = translations.find(
+					// 					(t) =>
+					// 						t.id === data._id ||
+					// 						t.id === data.name
+					// 				);
+					// 			} else {
+					// 				translation =
+					// 					translations[data._id] ||
+					// 					translations[data.name];
+					// 			}
+					// 			if (translation) {
+					// 				let translatedData = skillMapping.map(
+					// 					data,
+					// 					translation
+					// 				);
+					// 				return mergeObject(
+					// 					data,
+					// 					mergeObject(translatedData, {
+					// 						translated: true,
+					// 					})
+					// 				);
+					// 			}
+					// 		}
+
+					// 		let pack = game.babele.packs.find(
+					// 			(pack) =>
+					// 				pack.translated && pack.hasTranslation(data)
+					// 		);
+					// 		if (pack) {
+					// 			return pack.translate(data);
+					// 		}
+					// 		return data;
+					// 	});
+					// };
+				});
+				return data;
+			},
 		});
 	}
 });
